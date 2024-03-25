61cd51d6dcaff07605721eee3ab97fbc999f5224
Skle <vimalvenky24@gmail.com>
2024-03-24 22:46:11 -0500

    Initial Commit

---
+++ CS2CookiesSharedAPI.cs
@@ -0,0 +1,19 @@
+﻿using CounterStrikeSharp.API.Core;
+using MySqlConnector;
+
+namespace shared
+{
+    public interface ICS2CookiesShared
+    {
+        public Task<ICookie?> FindClientCookie(string key);
+        public Task<ICookie> RegClientCookie(string key);
+        public MySqlConnection GetConnection();
+    }
+
+    public interface ICookie
+    {
+        public Task<string?> Get(CCSPlayerController player);
+        public void Set(CCSPlayerController player, string value);
+    }
+
+}
